ff859d2bb9dcf08e9bf5244ca95fbc30b209db95
Tree <tree081996@gmail.com>
2017-09-28 00:38:45 +0800

    抓取动漫信息接近完成，写入数据库没写，js生成的数据抓取没没写

---
+++ AnimeSpider/Spider/AnimeSpider.py
@@ -0,0 +1,77 @@
+# -*- coding:utf-8 -*-
+
+import requests
+from bs4 import BeautifulSoup
+import bs4
+from Anime import Anime
+
+# 生成url链接
+# @param pageNum 访问的页数
+# @return 要访问的url 
+def generateUrl(pageNum):
+	return r'http://www.36dm.com/sort-2-' + str(pageNum) + r'.html'
+
+# 得到所有动漫的页数
+def getAllPagesNum():
+	response = visitUrl(r'http://www.36dm.com/sort-2-2.html')
+	soup = BeautifulSoup(response, 'html.parser')
+	lastPage = soup.find_all('a', attrs={'class':'pager-last active'})
+	return int(lastPage[0].string)
+
+# 访问url
+# @param url 要访问的url
+def visitUrl(url):
+	try:
+		r = requests.get(url)
+		r.raise_for_status()
+		return r.text
+	except:
+		print('网络错误')
+
+# 提取动漫信息，写入数据库
+# @param text 待处理的信息
+def handleUrl(text):
+	soup = BeautifulSoup(text, 'html.parser')
+	AnimeInfo = soup.find_all('tr', attrs={'class':'alt1'})
+	AnimeInfo.extend(soup.find_all('tr', attrs= {'class':'alt2'}))
+
+	for tr in AnimeInfo:
+		# 去掉非Tag子节点
+		childrenNodes = [item for item in tr.contents if type(item) is bs4.element.Tag]
+
+		AnimeDate = childrenNodes[0].string
+		AnimeType = childrenNodes[1].contents[0].string
+		AnimeName = childrenNodes[2].contents[0].string
+		AnimeSize = childrenNodes[3].string
+
+		# 这两个数据是js生成的，暂时不好提取
+		AnimeDownload = ''
+		AnimeFinish = ''
+
+		AnimeMagnet = getManget(r'http://www.36dm.com/' + childrenNodes[2].contents[1].get('href'))
+		anime = Anime(AnimeName, AnimeType, AnimeDate, AnimeSize, AnimeDownload, AnimeFinish, AnimeMagnet)
+
+		with open('data.txt', 'a') as f:
+			f.write(anime.getName())
+			f.write(anime.getMagnet())
+			f.write('\n')	
+
+# 提取网页中的磁力链接
+# @param url 待访问网页
+# @return 磁力链接
+def getManget(url):
+	text = visitUrl(url)
+	soup = BeautifulSoup(text, 'html.parser')
+	magnet = soup.find('a', id='magnet').get('href')
+	return magnet
+
+def main():
+	#pageNums = getAllPagesNum()
+
+	# 访问某一页
+	for pageNum in range(1, 2):
+		url = generateUrl(pageNum)
+		text = visitUrl(url)
+		handleUrl(text)
+
+main()
